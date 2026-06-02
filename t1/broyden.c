0b1093b87269a2482ff64f13c404274d5f573320
elisaeleoterio <elisaeleoterio@ufpr.br>
2026-06-02 10:35:07 -0300

    incios das otimizações

--- t1/broyden.c
+++ t1/broyden.c
@@ -11,7 +11,6 @@
 
 int main(int argc, char *argv[]) {
     
-    // Verifica se é para escrever em um arquivo ou para escrever dados por stdout
     FILE *output = stdout;
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-o") == 0) {
@@ -24,27 +23,18 @@ int main(int argc, char *argv[]) {
         }
     }
     
-    // Ler entradas
-    int n, max = MAX_ITER;
-
     // // Ler valores especificos do trbalho
-    // N: dimensão do SNLB
+
+    // n: dimensão do SNLB
     // x0: valor inicial das variáveis (um único, igual para todas as variáveis)
     // epsilon: tolerância a ser considerada nos critérios de parada do Método de Newton
-    // MAX: número máximo de iterações do Método de Newton
-    
-    float  xo, epsilon;
+    // max: número máximo de iterações do Método de Newton
+    int n, max = MAX_ITER;
+    float xo, epsilon;
 
-    // fprintf(output, "Insira o tamanho da matriz: ");
     scanf("%d %f %f", &n, &epsilon, &xo);
 
-    // // fprintf(output, "Insira o valor inicial das variáveis (x0): ");
-    // scanf("%f", &xo);
-
-    // // fprintf(output, "Insira o valor de epsilon: ");
-    // scanf("%f", &epsilon);
-
-    // Cria vetor das variáveis iniciais
+    // Cria vetor das variáveis X iniciais
     double *x = malloc(n * sizeof(double));
     verifica_alocacao(x, output);
 
@@ -53,7 +43,7 @@ int main(int argc, char *argv[]) {
         x[i] = xo;
     }
 
-    fprintf(output, "Vetor X  inicial com x0.\n");
+    fprintf(output, "Vetor X inicial com x0.\n");
     imprimirVetor(output, x, n);
     fprintf(output, "\n");
 
--- t1/matriz.c
+++ t1/matriz.c
@@ -7,53 +7,59 @@
 #include "utils.h"
 #include "matriz.h"
 
-// Monta a matriz de Broyden a partir do vetor X seguindo a estrutura
+#define TAM_BANDA_JACOBIANA 3
+#define TAM_SUPERDIAGONAL 1
+#define TAM_SUBDIAGONAL 1
+
+// Monta a matriz de Broyden resolvida a partir do vetor X seguindo a estrutura
 // f1(x) = -2x1² + 3x1 - 2x2 + 1
 // fi(x) = -2xi² + 3xi - xi-1 - 2xi+1 + 1, se 2<= i<= (n-1)
 // fn(x) = -2xn² + 3xn - xn-1
+
 double *criaEResolveBroyden(int n, double *x, FILE *output) {
 
-    // TODO Verificar se é a melhor maneira de alcoar a matriz
     double *F = malloc(n * sizeof(double));
     verifica_alocacao(F, output);
 
-    // Monta a matriz
     F[0] = (3 - 2*x[0])*x[0] - 2*x[1] + 1;
+
     for(int i = 1; i < n - 1; i++) {
         F[i] = (3 - 2*x[i])*x[i] - x[i-1] - 2*x[i+1] + 1;
     }
+
     F[n-1] = (3 - 2*x[n-1])*x[n-1] - x[n-2] + 1;
     
     return F;
 }
 
-// Calcula a matriz com as derivadas parciais da matriz de broyden
-double **calcularJacobiana(double *x, int n, rtime_t *tempJacobiana) {
+// Calcula a matriz Jacobiana com as derivadas parciais da matriz de Broyden
+
+double *calcularJacobiana(double *x, int n, rtime_t *tempJacobiana) {
+    
     char marker_jacob[50];
     snprintf(marker_jacob, sizeof(marker_jacob), "Jacobiana_%d", n);
-    // TODO Ver se calculo o custo por iteração ou a soma das iterações mesmo
     LIKWID_MARKER_START(marker_jacob);
     rtime_t aux = timestamp();
 
-    // TODO Verificar forma de alocar a matriz
-    // Alocação dinâmica da matriz jacobiana
-    double **J = calloc(n, sizeof(double*));
-    for (size_t i = 0; i < n; i++) {
-        J[i] = calloc(n, sizeof(double));
-    }
+    // Tridiagonal de Broyden => Tam = 3
+    int k1 = TAM_SUBDIAGONAL;
+    int k2 = TAM_SUPERDIAGONAL;
+    int tam = TAM_BANDA_JACOBIANA; 
 
-    // TODO Ver como otimizar esse for (matriz jacobiana de Broyden é uma matriz k-diagonal, talvez isso permita alguma outra otimização)
-    // Criar matriz jacobiana
-    J[0][0] = 3 - 4*x[0];
-    J[0][1] = -2;
-    
+    // Vetor para armazenar as 3-diagonais diferentes de 0 da matriz Jacobiana
+    double *J = malloc(n * tam * sizeof(double));
+
+    // Conversor das coordenadas indice = i * tam + (j − i + k1​)
+
+    J[0] = 3 - 4*x[0];
+    J[1 + k1] = -2; //(2)
     for (int i = 1; i < n - 1; i++) {
-        J[i][i-1] = -1;
-        J[i][i] = 3 - 4*x[i+1];
-        J[i][i+1] = -2;
+        J[i * tam ] = -1;
+        J[i * tam + k1] = 3 - 4*x[i+1];
+        J[i * tam + 1 + k1] = -2;
     }
-    J[n-1][n-2] = -1;
-    J[n-1][n-1] = 3 - 4*x[n-1];
+    J[(n - 1) * tam + k1 - 1] = -1;
+    J[(n - 1) * tam + k1] = 3 - 4*x[n-1];
     
     LIKWID_MARKER_STOP(marker_jacob);
     aux = timestamp() - aux;
@@ -62,56 +68,80 @@ double **calcularJacobiana(double *x, int n, rtime_t *tempJacobiana) {
 }
 
 
-// Faz a resolução do sistema linear usando Eliminação de Gauss e Retrossubstituição
-// TODO Otimizar usando Gauss-Seidel ou Gauss-Jacobi por ser uma matriz k-diagonal
-double *resolverSistemaLinear(double **J, int n, double *x, rtime_t *tempSL, FILE *output) {
+// TODO Otimizar usando Gauss-Seidel por ser uma matriz tridiagonal
+double *resolverSistemaLinear(double *J, int n, double *x, rtime_t *tempSL, FILE *output) {
     
     char marker_sl[50];
     snprintf(marker_sl, sizeof(marker_sl), "Linear_%d", n);
     LIKWID_MARKER_START(marker_sl);
     rtime_t aux = timestamp();
 
-    // Vetor de coeficientes independentes == Matriz de Broyden em X negados 
-    double *F = criaEResolveBroyden(n, x, output);
-    for (size_t i = 0; i < n; i++) {
-        F[i] = (-1) * F[i];
-    }
+
+    // J[0] = 3 - 4*x[0];; // Diagonal
+    // J[1 + k1] = -2;;    // Superdiagonal
+
+    // J[i * tam ] = -1; // Subdiagonal
+    // J[i * tam + k1] = 3 - 4*x[i+1]; // Diagonal
+    // J[i * tam + 1 + k1] = -2; // Superdiagnal
+
+    // J[(n - 1) * tam + k1 - 1] = -1; // Subdiagonal
+    // J[(n - 1) * tam + k1] = 3 - 4*x[n-1]; // Diagonal
+
+//         void gaussSeidel (double *d, double *a, double *c,
+// double *b, double *x, uint n, double tol)
+// {
+// double erro = 1.0 + tol;
+// while (erro < tol) { // 5(n-2)+6 ≈ 5n FLOPs / iteração
+// x[ 0 ] = (b[ 0 ] – c[ 0 ] * x[ 1 ]) / d[ 0 ];
+// for (int i=1; i < n-1; ++i)
+// x[ i ] = (b[ i ] – a[ i-1 ] * x[ i-1] – c[ i ] * x[ i+1 ]) / d[ i ];
+// x[ n-1 ] = (b[ n-1 ] – a[ n-2 ] * x[ n-2 ] ) / d[ n-1 ];
+// // Calcula erro == norma máxima de ( x(k) – x(k-1) )
+// }
+
     
-    // Eliminação de Gauss
-    for (int i = 0; i < n; ++i) {
-        int iMax = i;
-        for (int k = i + 1; k < n; ++k)
-            if (J[k][i] > J[iMax][i])
-                iMax = k;
-        if (iMax != i)
-        {
-            double *tmp, aux;
-            tmp = J[i];
-            J[i] = J[iMax];
-            J[iMax] = tmp;
 
-            aux = F[i];
-            F[i] = F[iMax];
-            F[iMax] = aux;
-        }
+    // // Vetor de coeficientes independentes == Matriz de Broyden em X negados 
+    // double *F = criaEResolveBroyden(n, x, output);
+    // for (size_t i = 0; i < n; i++) {
+    //     F[i] = (-1) * F[i];
+    // }
+    
+    // // Eliminação de Gauss
+    // for (int i = 0; i < n; ++i) {
+    //     int iMax = i;
+    //     for (int k = i + 1; k < n; ++k)
+    //         if (J[k][i] > J[iMax][i])
+    //             iMax = k;
+    //     if (iMax != i)
+    //     {
+    //         double *tmp, aux;
+    //         tmp = J[i];
+    //         J[i] = J[iMax];
+    //         J[iMax] = tmp;
 
-        for (int k = i + 1; k < n; ++k) {
-            double m = J[k][i] / J[i][i];
-            J[k][i] = 0.0;
-            for (int j = i + 1; j < n; ++j)
-                J[k][j] -= J[i][j] * m;
-            F[k] -= F[i] * m;
-        }
-    }
+    //         aux = F[i];
+    //         F[i] = F[iMax];
+    //         F[iMax] = aux;
+    //     }
 
-    double *delta = malloc(n * sizeof(double));
-    // Retrossubstituição
-    for (int i = n - 1; i >= 0; --i) {
-        delta[i] = F[i];
-        for (int j = i + 1; j < n; ++j)
-            delta[i] -= J[i][j] * delta[j];
-        delta[i] /= J[i][i];
-    }
+    //     for (int k = i + 1; k < n; ++k) {
+    //         double m = J[k][i] / J[i][i];
+    //         J[k][i] = 0.0;
+    //         for (int j = i + 1; j < n; ++j)
+    //             J[k][j] -= J[i][j] * m;
+    //         F[k] -= F[i] * m;
+    //     }
+    // }
+
+    // double *delta = malloc(n * sizeof(double));
+    // // Retrossubstituição
+    // for (int i = n - 1; i >= 0; --i) {
+    //     delta[i] = F[i];
+    //     for (int j = i + 1; j < n; ++j)
+    //         delta[i] -= J[i][j] * delta[j];
+    //     delta[i] /= J[i][i];
+    // }
 
     LIKWID_MARKER_STOP(marker_sl);
     aux = timestamp() - aux;
